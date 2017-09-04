44f46a5e6cc79f53590606e8513bf2134823da25
James Weber <jpweber@gmail.com>
2017-09-03 22:02:03 -0400

    completed program

--- kata04/main02.go
+++ kata04/main02.go
@@ -11,8 +11,16 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func spread(values []int) float64 {
+	spread := float64(values[0]) - float64(values[1])
+	return spread
+}
 
 func main() {
 
@@ -23,7 +31,47 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	var diff float64
+	var curSpread float64
+	var team string
+	var myTeam string
+	i := 0
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		if i == 0 {
+			i++
+			continue
+		}
+		// fmt.Println(strings.Fields(scanner.Text()))
+		line := strings.Fields(scanner.Text())
+		// fmt.Println(line)
+		if len(line) > 8 {
+			pos1, _ := strconv.Atoi(line[6])
+			pos2, _ := strconv.Atoi(line[8])
+
+			if pos1 == 0 && pos2 == 0 {
+				continue
+			}
+			team = line[1]
+
+			diff = spread([]int{pos1, pos2})
+			if i == 1 {
+				curSpread = diff
+				myTeam = team
+			} else if math.Abs(diff) < math.Abs(curSpread) {
+				curSpread = diff
+				myTeam = team
+
+			}
+		}
+		fmt.Println("curspread", curSpread)
+		fmt.Println("team", team)
+		i++
 	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Smallest Diff is:", math.Abs(curSpread))
+	fmt.Println("Team:", myTeam, "\n")
 }
